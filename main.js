27da4168a1cfeeda942825d4af8f590d91735d7f
Sara Orciuoli <orciuoli.sara@gmail.com>
2023-04-21 23:08:35 +0200

    Gestione funzioni Play/Pause

--- main.js
+++ main.js
@@ -10,46 +10,55 @@
 // - Durata brano
 
 // DATI DI PARTENZA:
-    // I brani all'interno del nostro MUSIC PLAYER
-    let tracks = [
-        {'url': './canzoni progetto/Backstreet Boys - I Want It That Way.mp3', 'cover': './cover/cover-1.jpg', 'artist': 'Backstreet Boys', 'title': 'I Want It That Way'},
-        {'url': './canzoni progetto/Caparezza - Vieni a ballare in Puglia.mp3', 'cover': './cover/cover-2.jpg', 'artist': 'Caparezza', 'title': 'Vieni a ballare in Puglia'},
-        {'url': './canzoni progetto/El sueño de Morfeo- Esta soy yo.mp3', 'cover': './cover/cover-3.jpg', 'artist': 'El sueño de Morfeo', 'title': 'Esta soy yo'},
-        {'url': './canzoni progetto/I Am What I Am (2014 Club Version).mp3', 'cover': './cover/cover-4.jpg', 'artist': 'Gloria Gaynor', 'title': 'I Am What I Am'},
-        {'url': './canzoni progetto/Pitbull - 3 to Tango.mp3', 'cover': './cover/cover-5.jpg', 'artist': 'Pitbull', 'title': '3 to Tango'},
-        {'url': './canzoni progetto/Salsa_Con_Coco.mp3', 'cover': './cover/cover-6.jpg', 'artist': 'Pochy y Su Cocoband', 'title': 'Salsa_Con_Coco'},
-        {'url': './canzoni progetto/Shania_Twain_-_Man_I_Feel_Like_A_Woman.mp3', 'cover': './cover/cover-7.jpg', 'artist': 'Shania Twain', 'title': 'Man_I_Feel_Like_A_Woman'},
-        {'url': './canzoni progetto/Sud sound system - Le radici ca tieni con testo.mp3', 'cover': './cover/cover-8.jpg', 'artist': 'Sud sound system ', 'title': 'Le radici ca tieni con'},
-        {'url': './canzoni progetto/Sunrise NORAH JONES.mp3', 'cover': './cover/cover-9.jpg', 'artist': 'NORAH JONES', 'title': 'Sunrise'},
-        {'url': './canzoni progetto/Alanis-morissette-thank-you-lyrics-youtubemp3free.org.mp3', 'cover': './cover/cover-10.jpg', 'artist': 'Alanis Morissette', 'title': 'Thank you'}
-    ];
+// I brani all'interno del nostro MUSIC PLAYER
+let tracks = [
+    { 'url': './canzoni progetto/Backstreet Boys - I Want It That Way.mp3', 'cover': './cover/cover-1.jpg', 'artist': 'Backstreet Boys', 'title': 'I Want It That Way' },
+    { 'url': './canzoni progetto/Caparezza - Vieni a ballare in Puglia.mp3', 'cover': './cover/cover-2.jpg', 'artist': 'Caparezza', 'title': 'Vieni a ballare in Puglia' },
+    { 'url': './canzoni progetto/El sueño de Morfeo- Esta soy yo.mp3', 'cover': './cover/cover-3.jpg', 'artist': 'El sueño de Morfeo', 'title': 'Esta soy yo' },
+    { 'url': './canzoni progetto/I Am What I Am (2014 Club Version).mp3', 'cover': './cover/cover-4.jpg', 'artist': 'Gloria Gaynor', 'title': 'I Am What I Am' },
+    { 'url': './canzoni progetto/Pitbull - 3 to Tango.mp3', 'cover': './cover/cover-5.jpg', 'artist': 'Pitbull', 'title': '3 to Tango' },
+    { 'url': './canzoni progetto/Salsa_Con_Coco.mp3', 'cover': './cover/cover-6.jpg', 'artist': 'Pochy y Su Cocoband', 'title': 'Salsa_Con_Coco' },
+    { 'url': './canzoni progetto/Shania_Twain_-_Man_I_Feel_Like_A_Woman.mp3', 'cover': './cover/cover-7.jpg', 'artist': 'Shania Twain', 'title': 'Man_I_Feel_Like_A_Woman' },
+    { 'url': './canzoni progetto/Sud sound system - Le radici ca tieni con testo.mp3', 'cover': './cover/cover-8.jpg', 'artist': 'Sud sound system ', 'title': 'Le radici ca tieni con' },
+    { 'url': './canzoni progetto/Sunrise NORAH JONES.mp3', 'cover': './cover/cover-9.jpg', 'artist': 'NORAH JONES', 'title': 'Sunrise' },
+    { 'url': './canzoni progetto/Alanis-morissette-thank-you-lyrics-youtubemp3free.org.mp3', 'cover': './cover/cover-10.jpg', 'artist': 'Alanis Morissette', 'title': 'Thank you' }
+];
 
 
-    // Catturare il contenitore della colonna 2
-    let wrapped = document.querySelector('#wrapped');
-    //  Sezione Audio e Cover 
+// Catturare il contenitore della colonna 2
+let wrapped = document.querySelector('#wrapped');
+// Inizializzare il tag audio, per poterlo richiamare ogni volta che cambio audio 
+let audio = null;
 
-    function createCover() {
-        let div = document.createElement('div');
-        div.classList.add('col-12', 'col-md-3', 'col-lg-5', 'd-flex', 'justify-content-center');
-        div.innerHTML = `  
-        <img class="img-rounded" src="./cover/cover-1.jpg" alt="">
+// Contenitore della mia lista
+let counterTrack = 0;
+
+//  Sezione Audio e Cover 
+function createCover() {
+    let div = document.createElement('div');
+    div.classList.add('col-12', 'col-md-3', 'col-lg-5', 'd-flex', 'justify-content-center');
+    div.innerHTML = `  
+        <img class="img-rounded" src="${tracks[counterTrack].cover}" alt="">
          
         <audio preload="metadata">
-            <source src="./canzoni progetto/Backstreet Boys - I Want It That Way.mp3" type="audio/mpeg">
+            <source src="${tracks[counterTrack].url}" type="audio/mpeg">
         </audio>
         `;
 
-        wrapped.appendChild(div);
-    }
+    wrapped.appendChild(div);
 
-    function createInfoTrack(){
-        let div = document.createElement('div');
-        div.classList.add('col-12', 'col-md-5', 'col-lg-6', 'MyMargin');
-        div.innerHTML = `
+    // Catturare l'elemento HTML ogni volta che lo richiamo
+    audio = document.querySelector('audio');
+}
+// Info Track
+
+function createInfoTrack() {
+    let div = document.createElement('div');
+    div.classList.add('col-12', 'col-md-5', 'col-lg-6', 'MyMargin');
+    div.innerHTML = `
         <!-- Sezione Info Track -->
-        <h2 class="text-center">Titolo del brano</h2>
-        <h3 class="text-center">Nome Artista</h3>
+        <h2 class="text-center">${tracks[counterTrack].title}</h2>
+        <h3 class="text-center">${tracks[counterTrack].artist}</h3>
 
         <div class="progress mt-5">
             <div class="progress-bar w-75" role="progressbar" aria-label="Basic example" aria-valuenow="75"
@@ -62,15 +71,36 @@
         </div>
 
         <div class="d-flex justify-content-between mt-3">
-            <button class="btn btn-outline-dark fs-4"><i class="fa-solid fa-backward"></i></button>
-            <button class="btn btn-outline-dark fs-4"><i class="fa-brands fa-google-play"></i></button>
-            <button class="btn btn-outline-dark fs-4"><i class="fa-solid fa-forward"></i></button>
+            <button id="btnBackward" class="btn btn-outline-dark fs-4"><i class="fa-solid fa-backward"></i></button>
+            <button id="btnPlay" class="btn btn-outline-dark fs-4"><i class="fa-brands fa-google-play"></i></button>
+            <button id="btnForward" class="btn btn-outline-dark fs-4"><i class="fa-solid fa-forward"></i></button>
         </div>`
 
-        wrapped.appendChild(div);
-    }
-    // Funzione che crea la Cover audio
-    createCover();
-    // Funzione che crea l'Info Track
-    createInfoTrack();
-    
+    wrapped.appendChild(div);
+
+    // Funzionalitá di: play, pause, prev, next
+    let btnBackward = document.querySelector('#btnBackward');
+    let btnPlay = document.querySelector('#btnPlay');
+    let btnForward = document.querySelector('#btnForward');
+
+    // Gestione funzioni Play/Pause
+    btnPlay.addEventListener('click', () => {
+
+        if (audio.paused) {
+            audio.play();
+            btnPlay.innerHTML = '<i class="fa-solid fa-pause"></i>';
+        } else {
+            audio.pause();
+            btnPlay.innerHTML = '<i class="fa-brands fa-google-play"></i>';
+
+        }
+        
+
+    })
+
+
+}
+// Funzione che crea la Cover audio
+createCover();
+// Funzione che crea l'Info Track
+createInfoTrack();
